f1ac61a83072b34fb6bf9a86426c2db70790b431
MoustafaAhmed12 <ma0558317@gmail.com>
2025-01-01 23:01:23 +0200

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  BASE_URL: 'http://psolve-001-site1.qtempurl.com',
+  BASE_URL: 'https://psolve-001-site1.qtempurl.com',
   BASE_URL_FRONT: 'https://psolve.netlify.app',
   // BASE_URL: 'https://icpc.runasp.net',
   // BASE_URL_FRONT: 'https://isc-app.netlify.app',
